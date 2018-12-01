acea24fb7190c417511c690646fc8ad570a66b46
Jef Meijvis <jef.meijvis@student.kuleuven.be>
2018-12-01 20:00:35 +0100

    Changed design

--- scripts/firebaseLoader.js
+++ scripts/firebaseLoader.js
@@ -1,30 +1,51 @@
-firebase.initializeApp({
-    apiKey: "AIzaSyA2-R-S3WayRd6tFteSuvfdG7q6GM12tbc",
-    authDomain: "websitelogger-ca5b6.firebaseapp.com",
-    projectId: "websitelogger-ca5b6",
-});
+window.onload = main;
+let p1,p2,p3,a,id,database;
+const project = "homepage";
 
-// Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
-
-// Disable deprecated features
-db.settings({
-    timestampsInSnapshots: true
-});
-
-
-getUserIP(logInfo);
-
-
-function logInfo(ip)
+function main()
 {
-    console.log(getToday());
-    db.collection("users-kerstbomen").add({
-        date: getToday(),
-        ip: ip
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-});
+    console.log("main");
+    // Main logic goes here
+    initVariables()
+    initFirebase()
+    logInfo();
+}
 
+function initVariables()
+{
+    p1 = "AIzaSyA2-R-S3WayRd";
+    p2 = "tFteSuvfd";
+    p3 = "G7q6GM12tbc";
+    a = p1 + 6 +  p2 + p3;
+    id = "ca5b6";
+}
+
+function initFirebase()
+{
+    firebase.initializeApp({
+        apiKey: a,
+        authDomain: "websitelogger-"+ id + ".firebaseapp.com",
+        projectId: "websitelogger-" + id,
+    });
+
+    // Initialize Cloud Firestore through Firebase
+    database = firebase.firestore();
+
+    // Disable deprecated features
+    database.settings({
+        timestampsInSnapshots: true
+    });
+}
+
+function logInfo()
+{
+    database.collection("users-" + project).add
+    (
+        {
+            date: new Date().toJSON()
+        }
+    ).then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    ;
 }
